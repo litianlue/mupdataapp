18fc38f940e1a3edcfeb82340564f6e2c3c93d85
litianlue <804765751@qq.com>
2020-05-25 14:19:46 +0800

    first commit

---
+++ app/src/main/java/com/hanma/mupdataapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.hanma.mupdataapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.hanma.mupdataapp.util.UpdateAppUtils;
+
+public class MainActivity extends AppCompatActivity {
+    //服务器apk path,这里放了百度云盘的apk 作为测试
+    String apkPath = "http://issuecdn.baidupcs.com/issue/netdisk/apk/BaiduNetdisk_7.15.1.apk";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void updateApp(View view) {
+        UpdateAppUtils.from(this)
+                .serverVersionCode(2)
+                .serverVersionName("2.0")
+                .apkPath(apkPath)
+                .update();
+    }
+
+    public void downloadByWeb(View view) {
+        UpdateAppUtils.from(this)
+                .serverVersionCode(2)
+                .serverVersionName("2.0")
+                .apkPath(apkPath)
+                .downloadBy(UpdateAppUtils.DOWNLOAD_BY_BROWSER)
+                .update();
+    }
+
+
+
+    public void forceUpdate(View view) {
+        UpdateAppUtils.from(this)
+                .serverVersionCode(2)
+                .serverVersionName("2.0")
+                .apkPath(apkPath)
+                .isForce(true)
+                .update();
+    }
+
+    public void checkByName(View view) {
+        UpdateAppUtils.from(this)
+                .checkBy(UpdateAppUtils.CHECK_BY_VERSION_NAME)
+                .serverVersionName("2.0")
+                .serverVersionCode(2)
+                .apkPath(apkPath)
+                .downloadBy(UpdateAppUtils.DOWNLOAD_BY_BROWSER)
+                .isForce(true)
+                .update();
+    }
+}
